655af6528785edaf00a24bb58bc40674eb95f069
Tejraj0319 <rajeshpatil0319@gmail.com>
2026-04-17 12:01:20 +0530

    toLowerCase

--- index.js
+++ index.js
@@ -395,3 +395,19 @@
 // }
 // console.log(firstUniqueChar("loveleetcode")); 
 
+
+// toLowerCase
+const toLowerCase = (str) => {
+    let result = "";
+    for (let i = 0; i < str.length; i++) {
+        let charCode = str.charCodeAt(i)
+        if (charCode >= 65 && charCode <= 90) {
+            result += String.fromCharCode(charCode + 32)
+        }
+        else {
+            result += str[i];
+        }
+    }
+    return result
+}
+console.log(toLowerCase("RAJESH aNANta paTIL"))
