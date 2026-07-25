f7b0b4afafcfe1bea7529787f22bc716b1dacd07
Tejraj0319 <rajeshpatil0319@gmail.com>
2026-07-25 12:12:21 +0530

    Equilibrium Index

--- tcsNqt.js
+++ tcsNqt.js
@@ -158,3 +158,24 @@ const leaders = (arr) => {
     return result;
 };
 console.log(leaders([16, 17, 4, 3, 5, 2]));
+
+
+
+// Equilibrium Index
+function equilibriumIndex(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let leftSum = 0;
+        let rightSum = 0;
+        for (let j = 0; j < i; j++) {
+            leftSum += arr[j];
+        }
+        for (let j = i + 1; j < arr.length; j++) {
+            rightSum += arr[j];
+        }
+        if (leftSum === rightSum) {
+            return i;
+        }
+    }
+    return -1;
+}
+console.log(equilibriumIndex([1, 7, 3, 6, 5, 6]));
